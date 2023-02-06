3da5366007ddd0859e27e56aa2d21e5c8a01c75b
Yuuki <52652158+rene-roid@users.noreply.github.com>
2023-02-06 12:10:28 +0100

    aç

--- accounts.php
+++ accounts.php
@@ -1,7 +1,7 @@
 <?php 
     require_once "libs/dbauth.php"; 
     include_once "utils/config.php";
-
+// 
     session_start();
 	if (!isset($_SESSION['username'])) {
 		header("Location: login.php");
