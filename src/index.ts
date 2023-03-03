11040a144ffb971975f2e249012cc24bea6c2e41
Alejandro Font <fontalejandro0@gmail.com>
2023-03-03 15:35:22 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import router from './routes/routes';
 import cors from 'cors';
+import morgam from 'morgan';
+import http from 'http';
 
 const app = express();
 const PORT = 3000;
+const server = http.createServer(app);
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 app.use(cors());
+app.use(morgam('dev'));
 app.use(router);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
