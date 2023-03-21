ad0bf6cfe72f8bb36ddab79621a4298137ea8657
Alejandro Font <fontalejandro0@gmail.com>
2023-03-21 10:09:43 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import router from './routes/routes';
 import cors from 'cors';
 import morgam from 'morgan';
 import http from 'http';
+import ngrok from 'ngrok';
+const PORT = 3000;
 
 const app = express();
-const PORT = 3000;
 const server = http.createServer(app);
 app.use(express.urlencoded({ extended: false }));
 
@@ -15,5 +16,14 @@ app.use(morgam('dev'));
 app.use(router);
 
 server.listen(PORT, () => {
-    console.log(`[server]: Server is running at http://localhost:${PORT}`);
+    console.log(`[server]: Server is running at https://localhost:${PORT}`);
 });
+
+(async function () {
+    const url = await ngrok.connect({
+        addr: PORT,
+        authtoken: "2NH23uGCm1SLgcV99MHYWzMgxwD_6TjhcteG2Tb9FMuL3gWNV",
+        
+    });
+    console.log(`[ngrok]: Server is running at ${url}`);
+})();
